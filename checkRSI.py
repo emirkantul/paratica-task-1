aadc32e5f00259ad78facae6e5845fdbbf951284
emirkantul <94080241+emirkantul@users.noreply.github.com>
2022-01-20 04:00:36 +0300

    Final update
    Web socket removed, now save_signals only work on request and no threads used. Endpoint functions code length minimized. Unnecessary global variables removed. README updated. Unused imports removed.

--- checkRSI.py
+++ checkRSI.py
@@ -1,6 +1,6 @@
-import json, os, datetime, talib, numpy
-from binance import ThreadedWebsocketManager, client
-from functools import partial
+import datetime, talib, numpy
+from signal import signal
+from binance import client
 from decouple import config
 
 closes_at = []
@@ -10,38 +10,8 @@ w_length = 14
 api_key = config('BINANCE_API')
 api_secret = config('BINANCE_SECRET')
 
-# handle incoming messages
-def handle_message(creatable_signals, msg):    
-    candle = msg['k']
-    is_candle_closed = candle['x']
-
-    if is_candle_closed:
-        close_time_pair = []
-        signal = []
-        close_time_pair.append(float(candle['c']))
-        close_time_pair.append(candle['t'])
-
-        closes_at.pop(0) # remove first element (oldest) and append new one
-        closes_at.append(close_time_pair)
-
-        np_closes = numpy.array([item[0] for item in closes_at])
-        rsi = talib.RSI(np_closes, w_length)
-        
-        # catch signal
-        if rsi[-3] != None:
-            if rsi[-3] < 40 and rsi[-2] > 40:
-                signal.append(msg['s'])
-                signal.append(datetime.utcfromtimestamp(candle['t']).strftime('%Y-%m-%d %H:%M:%S'))
-                signal.append(rsi[-3])
-                signal.append(rsi[-2])
-                signal.append(closes_at[-2][0])
-                creatable_signals.append(signal)
-                
-                
-
-
-
-def checkRSI(parity_symbol, creatable_signals, time_interval = '4h', window_length = 14):
+def checkRSI(parity_symbol, time_interval = '4h', window_length = 14):
+    creatable_signals = []
     closes_at = []
     w_length = window_length
 
@@ -52,25 +22,33 @@ def checkRSI(parity_symbol, creatable_signals, time_interval = '4h', window_leng
     client_ = client.Client(api_key=api_key, api_secret=api_secret)
     klines = client_.get_historical_klines(symbol=parity_symbol, interval=time_interval, start_str=str(start_utc))
 
-    last_w = klines[-(window_length + 2):]
+    # get last window_length + 3 (to check rsi[-2] and rsi[-3] values and to create a signal on current last closed candle)
+    last_w = klines[-(window_length + 3):]
     for candle in last_w:
         pair = []
         pair.append(float(candle[4]))
         pair.append(candle[0])
         closes_at.append(pair)
 
+    # calculate rsi
     np_closes = numpy.array([item[0] for item in closes_at])
     rsi = talib.RSI(np_closes, w_length)
     print("here is the last two days RSI to check if next candle can make a signal")
     print(rsi)
 
-    twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
-    twm.start()
-
-    f = partial(handle_message, creatable_signals)
-    twm.start_kline_socket(callback=f, symbol=parity_symbol, interval=time_interval)
-
-    twm.join()
-
+    signal = []
+    
+    # catch signal
+    if rsi[-3] < 40 and rsi[-2] > 40:
+        signal.append(parity_symbol)
+        signal.append(datetime.utcfromtimestamp(candle['t']).strftime('%Y-%m-%d %H:%M:%S'))
+        signal.append(rsi[-3])
+        signal.append(rsi[-2])
+        signal.append(closes_at[-2][0])
+        creatable_signals.append(signal)
+        print("catched signal:", signal)
+    
+    return creatable_signals
+            
 if __name__ == "__main__":
    checkRSI("FTMUSDT", [])
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from typing import Optional
-from numpy.core.multiarray import array
-import psycopg2, uvicorn, threading, addToDb, checkRSI
+import psycopg2, uvicorn, addToDb, checkRSI
 from fastapi import FastAPI
 from decouple import config
 
-
-started_checking = False # to check if already started checking signals
 signals_created = [] # to hold created signals to add later
-last_parity = "" # to keep check signal parity_symbol changes
+
+# saves all signals with given parity symbol to db
+def saveSignals(parity_symbol):
+    for signal in signals_created:
+        if signal[0] == parity_symbol:
+            addToDb.addRecord(signal[0], signal[1], signal[2], signal[3], signal[4])
+            signals_created.remove(signal)
+
 postgres_pass = config('POSTGRES_PASS')
+
 con = psycopg2.connect(database="postgres", user="postgres", password=postgres_pass, host="127.0.0.1", port="5432")
 
 app = FastAPI()
@@ -20,7 +24,6 @@ def read_root():
 # select all from database where parity_symbol matches
 @app.get("/history/{parity_symbol}")
 def read_parity_history(parity_symbol: str):
-    global con
     cur = con.cursor()
     sql_command = f"SELECT pair_symbol, signal_date, rsi_2, rsi_1, previous_candle from RSI_SIGNAL where pair_symbol=\'{parity_symbol}\'"
     cur.execute(sql_command)
@@ -30,7 +33,6 @@ def read_parity_history(parity_symbol: str):
 # select all from database
 @app.get("/history/")
 def read_history():
-    global con
     cur = con.cursor()
     sql_command = "SELECT pair_symbol, signal_date, rsi_2, rsi_1, previous_candle from RSI_SIGNAL"
     cur.execute(sql_command)
@@ -40,52 +42,34 @@ def read_history():
 # add all instances from singals_created to database where parity_symbols match
 @app.put("/save_signals/{parity_symbol}")
 def save_signal(parity_symbol: str):
-    global signals_created
-    done = False
     message = "no record to add"
-    try:
-        for signal in signals_created:
-            if signal[0] == parity_symbol:
-                addToDb.addRecord(signal[0], signal[1], signal[2], signal[3], signal[4])
-                done = True
-                signals_created.remove(signal)
-    except Exception:
-        message = "error occured"
-    if done:
-        message = "success"
+    if signals_created:
+        try:
+            saveSignals(parity_symbol)
+            message = "success"
+                    
+        except Exception:
+            message = "error occured"
+        
     return {"parity_symbol": parity_symbol, "message": message}
 
-
+# check if there is signals that can be represented to user
 @app.get("/check_signals/{parity_symbol}")
 def check_signal(parity_symbol: str):
-    global started_checking
-    global signals_created
-    global last_parity
-    message = "started to checking signals"
+    try:
+        signals_created = checkRSI.checkRSI(parity_symbol=parity_symbol)
 
-    # first time start checking with given parity. used thread for concurrency
-    if not started_checking:
-        try:
-            check_signals = threading.Thread(target=checkRSI.checkRSI, args=(parity_symbol, signals_created), daemon=True)
-            check_signals.start()
-            started_checking = True
-            last_parity = parity_symbol
-        except Exception:
-            message = "an error occured"
-            
-    # not first time check last_parity if we are trying to check the same parity_symbol
+    except Exception:
+        message = "an error occured while checking RSI"
+
+    if signals_created:
+        message = "catched a signal"
     else:
-        if not last_parity == parity_symbol:
-            try:
-                check_signals = threading.Thread(target=checkRSI.checkRSI, args=(parity_symbol, signals_created), daemon=True)
-                check_signals.start()
-                started_checking = True
-                last_parity = parity_symbol
-            except Exception:
-                message = "an error occured"
-        else:
-            message = "already started to checking this parity. here is the results"
+        message = "no signals to create"
+
     return {"parity_symbol": parity_symbol, "message": message, "signals_created": signals_created}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
